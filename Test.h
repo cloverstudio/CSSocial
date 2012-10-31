957c10d7afcb98e6da90710343a095df75980e75
Marko Hlebar <marko.hlebar@clover-studio.com>
2012-10-31 13:33:40 +0100

    CSSocial added new methods CSFacebook, CSTwitter

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  CSSocial
+//
+//  Created by marko.hlebar on 10/30/12.
+//  Copyright (c) 2012 Clover Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  CSSocial
+//
+//  Created by marko.hlebar on 10/30/12.
+//  Copyright (c) 2012 Clover Studio. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
